134f4dfc3360e880d3372d1fbdd7b700fea8ac57
BooraVinay <vinaykumarboora03@outlook.com>
2024-01-19 18:45:14 +0530

    adding files

---
+++ src/main/java/com/jobhunt/finder/POJO/JobForm.java
@@ -0,0 +1,15 @@
+package com.jobhunt.finder.POJO;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class JobForm {
+
+    private String jobTitle;
+    private String companyName;
+    private String jobLocation;
+    private String postingDate;
+    private String jobDescription;
+    private MultipartFile jobFile;
+
+
+}
